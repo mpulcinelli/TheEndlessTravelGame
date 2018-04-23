0e54e02940869acdd20e6ead0abe1816606e3d8b
Márcio Pulcinelli <mpulcinelli@gmail.com>
2018-04-22 23:34:52 -0300

    Inclusão de nova macro .

--- Source/TheEndlessTravel/GameHelpers/GameMacros.h
+++ Source/TheEndlessTravel/GameHelpers/GameMacros.h
@@ -5,3 +5,4 @@
 #include "CoreMinimal.h"
 
 #define PRINT_LOG(TEXT_TO_LOG) UE_LOG(LogTemp, Warning, TEXT(TEXT_TO_LOG));
+#define PRINT_LOG_2(TEXT_TO_LOG, PARAM) UE_LOG(LogTemp, Warning, TEXT(TEXT_TO_LOG), PARAM);
