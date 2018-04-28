a039a3e9a18cf6ee2045205d2c09c56d4563e81b
Márcio Pulcinelli <mpulcinelli@gmail.com>
2018-04-28 10:40:44 -0300

    Criação da classe de pickup para objetivo do jogo.

---
+++ Source/TheEndlessTravel/Pickups/PickupObjective.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickups/PickupBase.h"
+#include "PickupObjective.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEENDLESSTRAVEL_API APickupObjective : public APickupBase
+{
+	GENERATED_BODY()
+public:
+	APickupObjective();
+	
+protected:
+	
+	
+};
