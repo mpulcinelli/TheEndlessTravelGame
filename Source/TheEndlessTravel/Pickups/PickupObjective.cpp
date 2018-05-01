406b473540a8304792db71f3a5a024baa96cece0
Márcio Pulcinelli <mpulcinelli@gmail.com>
2018-04-30 22:54:20 -0300

    Codificação da classe.

--- Source/TheEndlessTravel/Pickups/PickupObjective.cpp
+++ Source/TheEndlessTravel/Pickups/PickupObjective.cpp
@@ -2,6 +2,9 @@
 
 #include "PickupObjective.h"
 #include <Components/StaticMeshComponent.h>
+#include "GameHelpers/GameMacros.h"
+#include "TheEndlessTravelGameMode.h"
+#include <Kismet/GameplayStatics.h>
 
 
 
@@ -9,4 +12,42 @@
 APickupObjective::APickupObjective()
 {
 
+}
+
+void APickupObjective::BeginPlay()
+{
+	Super::BeginPlay();
+	this->ChooseItemToSpawn();
+}
+
+void APickupObjective::OnPickUpMeshComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	Super::OnPickUpMeshComponentBeginOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+	ATheEndlessTravelGameMode* TheEndlessTravelGameMode;
+
+	TheEndlessTravelGameMode = Cast<ATheEndlessTravelGameMode>(UGameplayStatics::GetGameMode(GetWorld()));
+	
+	if (!ensure(TheEndlessTravelGameMode != nullptr)) return;
+	
+	OtherActor->GetComponentByClass(UStaticMeshComponent::StaticClass());
+
+	
+	if (OverlappedComponent->ComponentTags.Num() <= 0)return;
+
+	TheEndlessTravelGameMode->SetObjectiveItem(*OverlappedComponent->ComponentTags[0].ToString());
+
+	//PRINT_LOG_2("Chamando Over OnPickUpMeshComponentBeginOverlap: %s ", *OverlappedComponent->ComponentTags[0].ToString());
+}
+
+void APickupObjective::ChooseItemToSpawn()
+{
+	if (PickupItens.Num() > 0)
+	{
+		int randRange = FMath::RandRange(0, PickupItens.Num() - 1);
+		
+		if (PickupItens[randRange].MeshToPresent != nullptr) {
+			PickUpMeshComponent->SetStaticMesh(PickupItens[randRange].MeshToPresent);
+			PickUpMeshComponent->ComponentTags.Add(FName(*PickupItens[randRange].NomeItem));
+		}
+	}
 }
--- Source/TheEndlessTravel/Pickups/PickupObjective.h
+++ Source/TheEndlessTravel/Pickups/PickupObjective.h
@@ -6,9 +6,20 @@
 #include "Pickups/PickupBase.h"
 #include "PickupObjective.generated.h"
 
-/**
- * 
- */
+USTRUCT(BlueprintType)
+struct FItemToPickup
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString NomeItem; 
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UStaticMesh* MeshToPresent;
+};
+
+
 UCLASS()
 class THEENDLESSTRAVEL_API APickupObjective : public APickupBase
 {
@@ -19,7 +30,15 @@ public:
 protected:
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	TArray <class UStaticMesh*> ObjetoMesh;
+	TArray<FItemToPickup> PickupItens;
 
+	virtual void BeginPlay() override;
+	
+	UFUNCTION()
+	virtual void OnPickUpMeshComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult) override;
+
+private:
+
+	void ChooseItemToSpawn();
 
 };
