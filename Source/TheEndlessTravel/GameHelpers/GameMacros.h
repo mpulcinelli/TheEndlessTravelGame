4b828ce07f1866b957c11c77a3ccae72e459cdc6
Márcio Pulcinelli <mpulcinelli@gmail.com>
2018-05-02 23:31:47 -0300

    Inclusão de macro para log to screen

--- Source/TheEndlessTravel/GameHelpers/GameMacros.h
+++ Source/TheEndlessTravel/GameHelpers/GameMacros.h
@@ -2,7 +2,12 @@
 
 #pragma once
 
+#include "Engine.h"
 #include "CoreMinimal.h"
 
+// LOG PARA ARQUIVO
 #define PRINT_LOG(TEXT_TO_LOG) UE_LOG(LogTemp, Warning, TEXT(TEXT_TO_LOG));
 #define PRINT_LOG_2(TEXT_TO_LOG, PARAM) UE_LOG(LogTemp, Warning, TEXT(TEXT_TO_LOG), PARAM);
+
+// LOG PARA TELA
+#define PRINT_TO_SCREEN(TEXT_TO_PRINT) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT_TO_PRINT);
